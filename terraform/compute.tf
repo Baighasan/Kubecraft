0492433ba5a2aabf88b7fb6ac84192dc57bf9d5c
Baighasan <baig.hasan@outlook.com>
2026-02-07 15:31:12 -0500

    Completed compute, outputs and cloud init script

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,1 +1,27 @@
+resource "oci_core_instance" "kc_instance" {
+  compartment_id = var.compartment_ocid
+  availability_domain = data.oci_identity_availability_domain.ad.name
+  display_name = "kubecraft-instance"
+  shape = "VM.Standard.A2.Flex"
 
+  shape_config {
+    ocpus = 3
+    memory_in_gbs = 16
+  }
+
+  source_details {
+    source_type = "image"
+    source_id = data.oci_core_images.ubuntu_arm.images[0].id
+    boot_volume_size_in_gbs = 100
+  }
+
+  create_vnic_details {
+    subnet_id = oci_core_subnet.kc_subnet.id
+    assign_public_ip = true
+  }
+
+  metadata = {
+    ssh_authorized_keys = var.ssh_public_key
+    user_data = base64encode(file("cloud-init.yaml"))
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,19 @@
+output "instance_ip" {
+  description = "IP assigned to instance"
+  value = oci_core_instance.kc_instance.public_ip
+}
 
+output "ssh_command" {
+  description = "Command to ssh into instance"
+  value = "ssh ubuntu@${oci_core_instance.kc_instance.public_ip}"
+}
+
+output "kubeconfig_command" {
+  description = "SCP command to copy kubeconfig from instance"
+  value = "scp ubuntu@${oci_core_instance.kc_instance.public_ip}:/etc/rancher/k3s/k3s.yaml ~/.kube/kubecraft-config"
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint to access kubernetes cluster"
+  value = "https://${oci_core_instance.kc_instance.public_ip}:6443"
+}
