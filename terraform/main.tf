a7875e8ec98479c1cc61c20fca5cfeabb5426d94
Baighasan <baig.hasan@outlook.com>
2026-02-07 14:49:15 -0500

    Started writing terraform for deployment, finished main and variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,32 @@
+terraform {
+  required_version = ">= 1.0.0"
 
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "oci" {
+  tenancy_ocid      = var.tenancy_ocid
+  user_ocid         = var.user_ocid
+  fingerprint       = var.fingerprint
+  private_key_path  = var.private_key_path
+  region            = var.region
+}
+
+data "oci_identity_availability_domain" "ad" {
+  compartment_id  = var.tenancy_ocid
+  ad_number       = 2
+}
+
+data "oci_core_images" "ubuntu_arm" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = "Canonical Ubuntu"
+  operating_system_version = "22.04"
+  shape                    = "VM.Standard.A2.Flex"
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,55 @@
+variable "tenancy_ocid" {
+  description = "OCI tenancy OCID"
+  type        = string
+  sensitive   = true
+}
 
+variable "user_ocid" {
+  description = "OCI user OCID for API authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "fingerprint" {
+  description = "API key fingerprint"
+  type        = string
+  sensitive   = true
+}
+
+variable "private_key_path" {
+  description = "Path to OCI API private key file"
+  type        = string
+}
+
+variable "compartment_ocid" {
+  description = "OCI compartment OCID for resource placement"
+  type        = string
+}
+
+variable "region" {
+  description = "OCI region to deploy in"
+  type        = string
+  default     = "us-ashburn-1"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for instance access"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "IP address for SSH whitelist"
+  type        = string
+}
+
+variable "node_port_min" {
+  description = "Start of NodePort range for Minecraft servers"
+  type        = number
+  default     = 30000
+}
+
+variable "node_port_max" {
+  description = "End of NodePort range for Minecraft servers"
+  type        = number
+  default     = 30099
+}
