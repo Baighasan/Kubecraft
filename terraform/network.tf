feba301e23492db326b55926d886e784439de87f
Baighasan <baig.hasan@outlook.com>
2026-02-07 15:07:39 -0500

    Completed writing network and security rules

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,1 +1,32 @@
+resource "oci_core_vcn" "kc_vcn" {
+  compartment_id  = var.compartment_ocid
+  cidr_blocks     = ["10.0.0.0/16"]
+  display_name    = "kubecraft-vcn"
+}
 
+resource "oci_core_internet_gateway" "kc_internet_gateway" {
+  compartment_id  = var.compartment_ocid
+  vcn_id          = oci_core_vcn.kc_vcn.id
+  display_name    = "kubecraft_igw"
+  enabled         = true
+}
+
+resource "oci_core_route_table" "kc_route_table" {
+  compartment_id  = var.compartment_ocid
+  vcn_id          = oci_core_vcn.kc_vcn.id
+  display_name    = "kubecraft-rt"
+
+  route_rules {
+    destination = "0.0.0.0/0"
+    network_entity_id = oci_core_internet_gateway.kc_internet_gateway.id
+  }
+}
+
+resource "oci_core_subnet" "kc_subnet" {
+  compartment_id = var.compartment_ocid
+  vcn_id = oci_core_vcn.kc_vcn.id
+  cidr_block = "10.0.1.0/24"
+  display_name = "kubecraft-public-subnet"
+  route_table_id = oci_core_route_table.kc_route_table.id
+  security_list_ids = [oci_core_security_list.kc_security_list.id]
+}
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,1 +1,37 @@
+resource "oci_core_security_list" "kc_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id = oci_core_vcn.kc_vcn.id
+  display_name = "kubecraft-security-list"
 
+  # One ingress security_rules block per inbound rule
+  ingress_security_rules {
+    protocol = "6"
+    source   = "${var.my_ip}/32"
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = 6443
+      max = 6443
+    }
+  }
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+    tcp_options {
+      min = var.node_port_min
+      max = var.node_port_max
+    }
+  }
+
+  # One egress_security_rules block for outbound
+  egress_security_rules {
+    protocol = "all"
+    destination = "0.0.0.0/0"
+  }
+}
